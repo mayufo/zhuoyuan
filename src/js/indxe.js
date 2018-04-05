6c9bac8ee2bb1667a8587314319ceaa0f4ac3f2b
mayufo <mayufo@aliyun.com>
2018-04-05 21:36:57 +0800

    当有子级菜单时才显示icon

--- src/js/indxe.js
+++ src/js/indxe.js
@@ -11,6 +11,9 @@
             this.scrollInit()
         },
         navInit () {
+            // 当有子菜单的时候显示icon
+            $('.nav-item').has('.children-list').find('i').show()
+
             $('.nav > li').on('mouseenter', function (e) {
                 $(e.currentTarget).addClass('active')
             })
